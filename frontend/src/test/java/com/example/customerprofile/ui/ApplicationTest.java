6c5540bfc20afceaaf572cf2d166e8a591d676de
Thomas Risberg <risbergt@vmware.com>
2022-12-02 17:03:35 -0500

    Add frontend starter

---
+++ frontend/src/test/java/com/example/customerprofile/ui/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.customerprofile.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
